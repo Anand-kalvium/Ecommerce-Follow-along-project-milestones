8d5b55e3067fe201deaa077c8d078724933469c9
Anand-kalvium <m.anand.s69@kalvium.community>
2025-02-04 15:59:53 +0530

    Milestone 12

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
-import "./App.css";
-import CreatePage from "./pages/createPage";
-import Home from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import './App.css'
+import LoginPage from './pages/LoginPage'
+import SignUpPage  from './pages/SignUpPage'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/createPage'
+import MyProducts from './pages/myProduct'
+import {Routes,Route,BrowserRouter} from 'react-router-dom'
+
 
 function App() {
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/create-product" element={<CreatePage/>} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path='/create-product' element={<CreatePage/>}/>
+        <Route path="/my-products" element={<MyProducts/>} />
+      </Routes>
+    </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
